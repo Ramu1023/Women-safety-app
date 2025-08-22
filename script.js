779be0381fc7a58c36a3bfd16d88e0f1e781ec2a
Ramu1023 <priyareddy1023123@gmail.com>
2025-08-22 17:13:51 +0530

    Update script.js
    Add API key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://cubnpddinqtubsptdipi.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1Ym5wZGRpbnF0dWJzcHRkaXBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NDA2MDMsImV4cCI6MjA3MTQxNjYwM30.Gy4XS-Br-DO8nl4Wq_qHhp2A9gd38raRvTokuqdfKqo';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // --- DOM ELEMENTS ---
